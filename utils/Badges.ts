913bd052a8740acc02d6727cda95923efa5424c4
noxyyk <setunskyto@gmail.com>
2023-08-25 16:53:02 +0200

    added new badge and new server

--- utils/Badges.ts
+++ utils/Badges.ts
@@ -1,13 +1,15 @@
 type BadgesObject = {
     developper: string,
     alphaTester: string,
-    staff: string
-    pro: string
+    staff: string,
+    pro: string,
+    vip: string
 }
 
 export const Badges: BadgesObject = {
-    developper: '1138771459938988052',
-    alphaTester: '1139658510343344291',
-    staff: '1139721194225016862',
-    pro: 'pro'
+    developper: '932651844428251195',
+    alphaTester: '1144641299748769864',
+    staff: '933990402078408715',
+    pro: 'pro',
+    vip: '932656700664717402'
 }
